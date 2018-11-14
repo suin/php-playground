2e4cf65f0075562a1245375046ca3ce1b1984c3f
suin <suinyeze@gmail.com>
2018-11-14 18:49:52 +0900

    feat: Add not working example to "Way to get all alphabetic chars"

--- WayToGetAllAlphabeticChars/WayToGetAllAlphabeticCharsTest.php
+++ WayToGetAllAlphabeticChars/WayToGetAllAlphabeticCharsTest.php
@@ -147,4 +147,74 @@ class WayToGetAllAlphabeticCharsTest extends TestCase
             \array_merge(\range('A', 'Z'), \range('a', 'z'))
         );
     }
+
+    /**
+     * @test
+     */
+    public function this_way_does_not_works(): void
+    {
+        self::assertSame(
+            [
+                'A',
+                'B',
+                'C',
+                'D',
+                'E',
+                'F',
+                'G',
+                'H',
+                'I',
+                'J',
+                'K',
+                'L',
+                'M',
+                'N',
+                'O',
+                'P',
+                'Q',
+                'R',
+                'S',
+                'T',
+                'U',
+                'V',
+                'W',
+                'X',
+                'Y',
+                'Z',
+                '[',
+                "\\",
+                ']',
+                '^',
+                '_',
+                '`',
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                'g',
+                'h',
+                'i',
+                'j',
+                'k',
+                'l',
+                'm',
+                'n',
+                'o',
+                'p',
+                'q',
+                'r',
+                's',
+                't',
+                'u',
+                'v',
+                'w',
+                'x',
+                'y',
+                'z',
+            ],
+            range('A', 'z')
+        );
+    }
 }
