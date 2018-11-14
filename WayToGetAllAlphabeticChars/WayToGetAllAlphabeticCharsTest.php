723a15db974644c5498cbceb8c310fc95c32c736
suin <suinyeze@gmail.com>
2018-11-14 18:40:32 +0900

    feat: Way to get all alphabetic chars

---
+++ WayToGetAllAlphabeticChars/WayToGetAllAlphabeticCharsTest.php
@@ -0,0 +1,150 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Suin\Playground\WayToGetAllAlphabeticChars;
+
+use PHPUnit\Framework\TestCase;
+
+class WayToGetAllAlphabeticCharsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function uppercase(): void
+    {
+        self::assertSame(
+            [
+                'A',
+                'B',
+                'C',
+                'D',
+                'E',
+                'F',
+                'G',
+                'H',
+                'I',
+                'J',
+                'K',
+                'L',
+                'M',
+                'N',
+                'O',
+                'P',
+                'Q',
+                'R',
+                'S',
+                'T',
+                'U',
+                'V',
+                'W',
+                'X',
+                'Y',
+                'Z',
+            ],
+            \range('A', 'Z')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function lowercase(): void
+    {
+        self::assertSame(
+            [
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                'g',
+                'h',
+                'i',
+                'j',
+                'k',
+                'l',
+                'm',
+                'n',
+                'o',
+                'p',
+                'q',
+                'r',
+                's',
+                't',
+                'u',
+                'v',
+                'w',
+                'x',
+                'y',
+                'z',
+            ],
+            \range('a', 'z')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function uppercase_and_lowercase(): void
+    {
+        self::assertSame(
+            [
+                'A',
+                'B',
+                'C',
+                'D',
+                'E',
+                'F',
+                'G',
+                'H',
+                'I',
+                'J',
+                'K',
+                'L',
+                'M',
+                'N',
+                'O',
+                'P',
+                'Q',
+                'R',
+                'S',
+                'T',
+                'U',
+                'V',
+                'W',
+                'X',
+                'Y',
+                'Z',
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                'g',
+                'h',
+                'i',
+                'j',
+                'k',
+                'l',
+                'm',
+                'n',
+                'o',
+                'p',
+                'q',
+                'r',
+                's',
+                't',
+                'u',
+                'v',
+                'w',
+                'x',
+                'y',
+                'z',
+            ],
+            \array_merge(\range('A', 'Z'), \range('a', 'z'))
+        );
+    }
+}
